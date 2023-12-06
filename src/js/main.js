f92d71968c8afb7ddff1826d4004c7ccb1381c1c
Piotr Poterek <peterpoterek0@gmail.com>
2023-12-07 00:58:34 +0100

    render images

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const fetchPixabayAPI = async () => {
+const gallery = document.querySelector('#gallery');
+
+const fetchPixabayAPI = async search => {
   const url = 'https://pixabay.com/api/';
   const apiKey = '41114633-51106070bf303d1c44ed5d4b9';
 
@@ -8,12 +10,24 @@ const fetchPixabayAPI = async () => {
     const res = await axios.get(url, {
       params: {
         key: apiKey,
+        q: search,
       },
     });
-    console.log(res.data);
+    console.log(res.data.hits);
+    renderImages(res.data.hits);
   } catch (err) {
     console.log(err);
   }
 };
 
-fetchPixabayAPI();
+fetchPixabayAPI('cats');
+
+const renderImages = images => {
+  images.forEach(image => {
+    console.log(image);
+    const img = document.createElement('img');
+    img.setAttribute('src', image.webformatURL);
+
+    gallery.append(img);
+  });
+};
