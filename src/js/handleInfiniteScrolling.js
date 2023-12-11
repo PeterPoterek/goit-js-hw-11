d053b71270246ad5ebd3b8431325de62aca1b794
Piotr Poterek <peterpoterek0@gmail.com>
2023-12-11 21:37:46 +0100

    scrollToImage

--- src/js/handleInfiniteScrolling.js
+++ src/js/handleInfiniteScrolling.js
@@ -10,11 +10,23 @@ import fetchPixaBayApi from './fetchPixaBayAPI.js';
 import { renderImages } from './renderImages.js';
 const loadMoreButton = document.querySelector('.load-more');
 
+const scrollToImage = () => {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+};
+
 const handleButtonClick = async () => {
   setCurrentPage(1);
   const data = await fetchPixaBayApi(currentSearch, currentPage, imagesPerPage);
 
   renderImages(data);
+  scrollToImage();
 };
 loadMoreButton.addEventListener('click', handleButtonClick);
 
